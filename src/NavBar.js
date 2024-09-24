789459a8c843d330f266b7665848646b679f6e88
hadi14250 <hadihk2k@gmail.com>
2024-09-24 12:44:29 +0400

    used usePrama hook from react-router to get the paramter in the link and use it as id

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,21 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav>
             <ul>
                 <li>
-                    <Link to="/" >Home</Link>
+                    <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="about" >About</Link>
+                    <Link to="/about">About</Link>
                 </li>
                 <li>
-                    <Link to="articles">Articles</Link>
+                    <Link to="/articles">Articles</Link>
                 </li>
             </ul>
         </nav>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,6 @@
 const HomePage = () => {
     return (
-        <h1>
-            This is the home page!
-        </h1>
+        <h1>This is the home page!</h1>
     );
 }
 
