c702f911b9c9261ddc7991d9d5c29d9ecd3457d9
hadi14250 <hadihk2k@gmail.com>
2024-09-24 18:09:23 +0400

    testing express with postman

--- react-blog-backend/src/server.js
+++ react-blog-backend/src/server.js
@@ -1,9 +1,16 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/hello", (req, res) => {
-    res.send("Hello!");
+app.post("/hello", (req, res) => {
+    console.log(req.body);
+    res.send(`Hello ${req.body.name}!`);
+});
+
+app.get("/hello/:name", (req, res) => {
+    const { name } = req.params;
+    res.send(`Hello ${name}!!`);
 });
 
 app.listen(8000, () => {
