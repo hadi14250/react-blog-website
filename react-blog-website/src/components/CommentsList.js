d661845390ba8d5386278183f14b1e39e403678c
hadi14250 <hadihk2k@gmail.com>
2024-09-25 17:44:15 +0400

    fixed bug where if comment was empty in db it is still showing on websirw

--- react-blog-website/src/components/CommentsList.js
+++ react-blog-website/src/components/CommentsList.js
@@ -2,6 +2,7 @@ const CommentsList = ({ comments }) => (
     <>
     <h3>Comments:</h3>
     {comments.map(comment => (
+        comment.postedBy && comment.text &&
         <div className="comment" key={comment.postedBy + ': ' + comment.text}>
             <h4>{comment.postedBy}</h4>
             <p>{comment.text}</p>
