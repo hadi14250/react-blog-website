0ee08e54e8a9ef553571b107a71d2889b8452a70
hadi14250 <hadihk2k@gmail.com>
2024-09-25 13:24:48 +0400

    pushing to work from another device

--- react-blog-backend/src/server.js
+++ react-blog-backend/src/server.js
@@ -3,12 +3,15 @@ import express from "express";
 let articlesInfo = [{
     name: "learn-react",
     upvotes: 0,
+    comments: [],
 }, {
     name: "learn-node",
     upvotes: 0,
+    comments: [],
 }, {
     name: "mongodb",
     upvotes: 0,
+    comments: [],
 },
 ];
 
@@ -22,6 +25,20 @@ app.put("/api/articles/:name/upvote", (req, res) => {
         article.upvotes += 1;
         res.send(`The ${name} article now has ${article.upvotes} upvote${article.upvotes === 1 ? "" : "s"}!`);
     } else {
+        res.send("That article doesn't exist");
+    }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    const article = articlesInfo.find(a => a.name === name);
+
+    if (article) {
+        article.comments.push({postedBy, text});
+        res.send(article.comments);
+    } else { 
         res.send("That article doesn't exist");
     }
 });
