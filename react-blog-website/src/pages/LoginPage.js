7853047866d8ba82984ba846def1a660906b4b61
hadi14250 <hadihk2k@gmail.com>
2024-09-26 17:21:27 +0400

    added loginpage firebase auth with error handeling

--- react-blog-website/src/pages/LoginPage.js
+++ react-blog-website/src/pages/LoginPage.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 
 const LoginPage = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+
+    const navigate = useNavigate();
+
+    const logIn = async () => {
+        try {
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            navigate("/articles");
+        } catch (e) {
+            setError(e.message);
+        }
+    }
      
     return (
         <>
@@ -22,7 +34,7 @@ const LoginPage = () => {
         value={password}
         onChange={e => setPassword(e.target.value)}
         type="password" />
-        <button>Login</button>
+        <button onClick={logIn}>Login</button>
         <Link to="/create-account">Don't have an account? Create one here</Link>
         </>
     );
