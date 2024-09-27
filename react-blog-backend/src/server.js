4b8c72056fc97ba236bbebadfb8eda1e3b177e5c
hadi14250 <hadihk2k@gmail.com>
2024-09-27 13:37:28 +0400

    added upvate protection so user can upvote only once

--- react-blog-backend/src/server.js
+++ react-blog-backend/src/server.js
@@ -4,24 +4,39 @@ import express from 'express';
 import { db, connectToDb } from './db.js';
 
 const credentials = JSON.parse(
-    fs.readFileSync("../credentials.json")
+    fs.readFileSync("./credentials.json")
 );
 
 admin.initializeApp( {
     credential : admin.credential.cert(credentials),
 } );
 
-
-
 const app = express();
 app.use(express.json());
 
+app.use( async (req, res, next) => {
+    const { authtoken } = req.headers;
+    if (authtoken) {
+        try {
+            req.user = await admin.auth().verifyIdToken(authtoken);
+        } catch (e) {
+             res.sendStatus(400);
+        }
+    }
+
+    next();
+} );
+
+
 app.get('/api/articles/:name', async (req, res) => {
     const { name } = req.params;
+    const { uid } = req.user;
 
     const article = await db.collection('articles').findOne({ name });
 
     if (article) {
+        const upvoteIds = article.upvoteIds || [];
+        article.canUpvote = uid && !upvoteIds.include(uid);
         res.json(article);
     } else {
         res.sendStatus(404);
